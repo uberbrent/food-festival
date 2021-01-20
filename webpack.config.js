9cc3e6dee1ad928c1fb11f303bd2ad89af9784b8
uberbrent <bmjohns1337@gmail.com>
2021-01-20 12:52:50 -0600

    adding optimization of images through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,38 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpack = require("webpack")
 const path = require("path");
 
 module.exports = {
-  entry: "./assets/js/script.js",
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+    },
   output: {
-    path: path.join(__dirname + "/dist"),
-    filename: "main.bundle.js"
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+        {
+            test: /\.jpg$/i,
+            use: [
+                {
+                    loader: 'file-loader'
+                }
+            ]
+        }
+    ]
   },
   plugins: [
   new webpack.ProvidePlugin({
     $: "jquery",
     jQuery: "jquery"
   }),
+  new BundleAnalyzerPlugin({
+      analyzerMode: 'static', 
+  })
   ],
   mode: "development"
 };
